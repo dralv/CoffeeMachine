c3245f78206c528897ad250444db5cedfe2e9974
Alvaro <dralvphd2@gmail.com>
2024-02-10 13:16:45 -0300

    Primeira versão

--- main.py
+++ main.py
@@ -1,16 +1,103 @@
-# This is a sample Python script.
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
+def show_report():
+    for resource in resources:
+        print(f"{resource} : {resources[resource]}")
 
+def client_order():
+    machine_on = True
+    while machine_on:
+        order = input("What would you like? (espresso/latte/cappuccino)")
+        if order =="off":
+            machine_on = False
+            break
+        if order=="report":
+            print_report()
+            continue
+        drink = MENU[order]
+        drink_ingredients = drink["ingredients"]
+        drink_cost = drink["cost"]
+        enough_resources = check_resources(drink_ingredients)
+        if enough_resources:
+            customer_money = insert_coins()
+            enough_money = check_money(customer_money,drink_cost)
+            if enough_money:
+                make_coffe(drink_ingredients)
+                print(f"Here is your {order}. Enjoy!")
+def insert_coins():
+    quarters = int(input("Insert quarters"))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    dimes = int(input("Insert dimes"))
 
+    nickels = int(input("Insert nickels"))
+
+    pennies = int(input("Insert pennies"))
+
+    dollars = (quarters*0.25) + (dimes*0.10) + (nickels*0.05) + (pennies*0.01)
+
+    print(f"You Inserted {dollars} dollars")
+    return dollars
+
+def check_resources(drink_ingredients):
+    for ingredient in drink_ingredients:
+        if drink_ingredients[ingredient] > resources[ingredient]:
+            print(f"Sorry there is not enough {ingredient}")
+            return False
+    return True
+
+def check_money(customer_money,drink_cost):
+    if customer_money < drink_cost:
+        print("Sorry that's not enough money. Money refunded")
+        return False
+    elif customer_money > drink_cost:
+        resources["money"] += drink_cost
+        change = str(round(customer_money - drink_cost, 2))
+        print(f"Here is ${change} dollars in change.")
+        return True
+    else:
+        resources["money"] += drink_cost
+        return True
+def make_coffe(drink_ingredients):
+    for  ingredient in drink_ingredients:
+        resources[ingredient]-= drink_ingredients[ingredient]
+def print_report():
+    for i in resources:
+        print(f"{i} : {resources[i]}")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    client_order()
+    print("Machine shutted down")
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
